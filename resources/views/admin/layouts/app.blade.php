e16f0cfd6ca78b9bf5a92e89f543da7318ce2c6c
Minh_Đức <ducle090891999@gmail.com>
2025-01-10 15:39:10 +0700

    f

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DUC COMPUTER</title>
-    <link rel="icon" href="{{ asset('icon/image.png') }}">
+    <link rel="icon" href="{{ secure_asset('icon/image.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
